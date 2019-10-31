c49e31e822200b24133210818ac13a380e559283
jaygraffagna <jaygraffagna10@gmail.com>
2019-10-30 22:12:29 -0500

    adding members

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,9 +9,37 @@ const About = center => (
     <Helmet title={'About Us'} />
     <Header title="About Us">Bettendorf Natural Resources Committee</Header>
     <Container center={center}>
-      <h3>
-        Working on adding the members info here
-      </h3>
+      <h2>
+        Members
+      </h2>
+      <div>
+        <div>
+          <b>Candace Egger</b>
+        </div>
+        Retired Bettendorf Teacher
+      </div>
+      <br/>
+      <div>
+        <div>
+          <b>Spencer Mesick</b>
+        </div>
+        Bettendorf High Environmental Science Teacher
+      </div>
+      <br/>
+      <div>
+        <div>
+          <b>Bill Collett</b>
+        </div>
+        Bettendorf High Environmental Science Teacher
+      </div>
+      <br/>
+      <div>
+        <div>
+          <b>Ann Kappeler</b>
+        </div>
+        Retired City Administator
+      </div>
+      <br/>
     </Container>
   </Layout>
 );
